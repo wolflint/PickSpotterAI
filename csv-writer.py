e259d9e68bf523543b54ecef8cd7db1e6a22531f
Marcin Wolf <wolflint@protonmail.com>
2020-05-06 20:01:49 +0100

    Edited csv-writer.py

--- csv-writer.py
+++ csv-writer.py
@@ -4,15 +4,17 @@ import csv
 with open('pick_locations.csv', "a", newline='') as location_file:
     fieldnames = ['id', 'img_name', 'pick_x', 'pick_y']
     filewriter = csv.DictWriter(location_file, fieldnames)
-    current_index = int(input('Current index: '))
+    next_index = int(input('Next index: '))
     file_prefix = input('Filename prefix character: ')
     file_suffix = '.jpg'
     num_of_new_images = int(input('How many new images? '))
+
+    current_index = next_index
     while current_index <= (current_index + num_of_new_images):
         file_index_string = '{:0>4}'.format(current_index)
         current_row = {
         'id': str(current_index),
-        'img_name': prefix + file_index_string + file_suffix,
+        'img_name': file_prefix + file_index_string + file_suffix,
         'pick_x': '0',
         'pick_y': '0',
         }
